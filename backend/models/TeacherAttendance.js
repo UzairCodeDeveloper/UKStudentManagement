dc095384cdce02e0be832f9e6d0ec3cd0c233172
Zunair Khawaja <102663927+khzunair@users.noreply.github.com>
2024-11-13 20:43:35 +0500

    Error Resolved

--- backend/models/TeacherAttendance.js
+++ backend/models/TeacherAttendance.js
@@ -7,20 +7,24 @@ const teacherAttendanceSchema = new Schema({
     type: Date,
     required: true
   },
-  teacher_id: {
-    type: Schema.Types.ObjectId, 
-    ref: 'Volunteer',  // Assuming a Teacher model exists
-    required: true
-  },
-  status: {
-    type: String,
-    enum: ['present', 'absent', 'leave'],
-    required: true
-  },
-  reason_for_leave: {
-    type: String,
-    default: ''
-  }
+  attendance:[
+    {
+      teacher_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Volunteer',
+        required: true
+      },
+      status: {
+        type: String,
+        enum: ['present', 'absent', 'leave'],
+        required: true
+      },
+      reason_for_leave: {
+        type: String,
+        default: ''
+      }
+    }
+  ]
 }, { timestamps: true });
 
 teacherAttendanceSchema.index({ teacher_id: 1, date: 1 }, { unique: true }); // Ensure teacher can't have multiple records on the same date
